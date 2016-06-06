f8ae38f86061e24c7da0cbbc97b129467138c2f5
pawelkula203996 <203996@student.pwr.edu.pl>
2016-06-06 16:48:10 +0200

    Update bankomat.h

--- bankomat.h
+++ bankomat.h
@@ -1,36 +1,40 @@
 #ifndef BANKOMAT_H
 #define BANKOMAT_H
 #include "Card.h"
+#include <iostream>
+using namespace std;
 
 class Bankomat
 {
 private:
-	//mozna usunac wedle zyczen
 	int gotowka;
-	Card karta1;
+	Card karta,zla,test;
 
 public:
-	Bankomat();
+	Bankomat(int g);
+	~Bankomat();
 
-	//Przypisuje zmiennej karta1 dane zwi¹zane z numerem karty zwraca true je¿eli operacja siê powiod³a
-	//dane uzyskuje z serwera
+	//Przypisuje zmiennej karta dane związane z numerem karty zwraca true jeżeli operacja się powiodła
+	//dane uzyskuje z serwera(plik.txt)
 	bool wczytaj_karte(int numer);
-	//sprawdza	czy karta1 jest kradziona (operacja karta1.sprawdz_kradziona())
+	//sprawdza	czy karta jest kradziona (operacja karta.sprawdz_kradziona())
 	bool czy_skradziona();
-	//prosi uzytkownika o podanie pinu 3x jezeli jest poprawne zwraca true, jezeli false po³yka karte
-	//(ustawia pola karta1 na null)
+	//prosi uzytkownika o podanie pinu 3x jezeli jest poprawne zwraca true, jezeli false połyka karte
+	//zamienia pola w karta na pola z karta zla
 	bool podaj_pin();
-	//sprawdza saldo na karcie (karta1.get_saldo()) nastepnie drukuje wartosc
-	string sprawdz_saldo();
-	//wyplaca pieniadze z karty w krokach:
+	//sprawdza saldo na karcie (karta.get_saldo()) nastepnie wyświetla wartosc
+	int sprawdz_saldo();
+	//wypłaca pieniądze z karty w krokach:
 	//1. prosi o wprowadzenie kwoty
-	//2. sprawdza dostepnosc salda (karta1.get_saldo) (jesli nie ma podaje saldo i prosi o podanie innej kwoty)
-	//2* sprawdza dostêpnosc gotowki w bankomacie
-	//3. odejmuje kwote wyplacana od salda
-	//3* odejmuje kwote wyplacana od gotowki
+	//2. sprawdza dostępność salda (karta.get_saldo) (jesli nie ma podaje saldo i prosi o podanie innej kwoty)
+	//2* sprawdza dostępność gotowki w bankomacie
+	//3. odejmuje kwote wypłacaną od salda
+	//3* odejmuje kwote wypłacaną od gotowki
 	//4. ustawia nowe saldo na karcie (karta.set_saldo)
-	//5. zapisuje dane karty do pliku i ustawia pola karty na null(usowa karte)
+	//5. zapisuje dane karty do pliku
 	void wyplac();
+	bool odczyt();  // bool - żeby sprawdził czy dana karta istnieje
+	void zapis();
 
 
 };
