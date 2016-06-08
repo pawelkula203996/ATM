4534c7869046c5a902a4fcde7eb630ad4b3c5cff
pawelkula203996 <203996@student.pwr.edu.pl>
2016-06-08 15:31:53 +0200

    Update bankomat.cpp

--- bankomat.cpp
+++ bankomat.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
@@ -19,15 +20,20 @@ Bankomat::Bankomat(int g){
 }
 Bankomat::~Bankomat(){}
 
-bool Bankomat::wczytaj_karte(int numer){
-
-	//wczytywanie z pliku (?)
-
-	if (numer = 1){ karta = test; }  //test programu, docelowo odczyt z pliku
+bool Bankomat::wczytaj_karte(int card_num){
 
 	int x = 0;  //zmienna potrzebna do sprawdzania pinu x3
 	int op;		//zmienna potrzebna do wyboru operacji
 
+	if (!odczyt(card_num)){               //wczytywanie z pliku
+
+		cout << "Taka karta nie istnieje!" << endl;
+		return false;
+	}
+
+	//if (card_num = 1)karta = test;  //test programu, docelowo odczyt z pliku
+
+	
 	if (czy_skradziona()){
 		karta = zla;
 		cout << "Karta skradziona!" << endl;
@@ -64,12 +70,12 @@ bool Bankomat::wczytaj_karte(int numer){
 		}
 		if (op == 3){
 			op = 0;
+			zapis(card_num);    //zapis do pliku 
 			return 0;
 		}
 	}
 
-	//zapis do pliku (?)
-
+		
 	return true;
 }
 		
@@ -116,4 +122,96 @@ void Bankomat::wyplac(){
 			cout << "Dostepne srodki: " << sprawdz_saldo() << endl;
 		}
 	}
+}
+
+void Bankomat::zapis(int card_num){
+
+	int i = 0;
+
+	string linia[5];
+	string kr = to_string(karta.sprawdz_kradziona());
+	string p  = to_string(karta.get_pin());
+	string zl = to_string(karta.get_saldo());
+
+	fstream plik;
+	plik.open("karty.txt", ios::in);  // | ios::app);
+
+
+	string line = to_string(card_num) + "|" + kr + "/" + p + "/" + zl;
+
+	while (getline(plik,linia[i])){
+
+		string delimiter_0 = "|";
+		
+		int nr = atoi((linia[i].substr(0, linia[i].find(delimiter_0))).c_str());
+
+		if (card_num == nr){
+
+			linia[i] = line;
+		}
+
+		i++;
+	}
+
+	plik.close();
+	plik.open("karty.txt", ios::out);
+
+	for (int k = 0; k < i; k++){
+
+		plik << linia[k] << endl;
+
+	}
+
+	plik.close();
+
+}
+
+bool Bankomat::odczyt(int card_num){
+
+	bool kr = 0;
+	int  p = 0;
+	int zl = 0;
+
+	string linia;
+	bool found = false;
+	fstream plik;
+
+	plik.open("karty.txt", ios::in);
+
+	if (plik.good() == false)
+	{
+		cout << "Plik nie istnieje!" << endl;
+		exit(0);
+	}
+
+	while (getline(plik, linia))
+	{
+		string delimiter_0 = "|";
+		string delimiter_1 = "/";
+		int nr = atoi((linia.substr(0, linia.find(delimiter_0))).c_str());    //kr = atoi(linia.c_str()); break; (konwersja)
+
+
+		linia.erase(0, linia.find(delimiter_0) + delimiter_0.length());
+
+		if (nr == card_num)
+		{
+			found = true;
+			kr = atoi(linia.substr(0, linia.find(delimiter_1)).c_str());
+			linia.erase(0, linia.find(delimiter_1) + delimiter_1.length());
+			p = atoi(linia.substr(0, linia.find(delimiter_1)).c_str());
+			linia.erase(0, linia.find(delimiter_1) + delimiter_1.length());
+			zl = atoi(linia.substr(0).c_str());
+			linia.erase(0);
+		
+			break; // no bo jak znalazl juz nasza karte to nie musi szukac
+		}
+
+	}
+
+	plik.close();
+
+	if(found)karta = Card(kr, p, zl);
+	
+	return found;
+
 }
