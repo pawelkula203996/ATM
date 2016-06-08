7698a4f09474b8bd6f38b4a9d7d9342bb76c5a0c
pawelkula203996 <203996@student.pwr.edu.pl>
2016-06-08 15:32:19 +0200

    Update bankomat.h

--- bankomat.h
+++ bankomat.h
@@ -16,7 +16,7 @@ public:
 
 	//Przypisuje zmiennej karta dane związane z numerem karty zwraca true jeżeli operacja się powiodła
 	//dane uzyskuje z serwera(plik.txt)
-	bool wczytaj_karte(int numer);
+	bool wczytaj_karte(int card_num);
 	//sprawdza	czy karta jest kradziona (operacja karta.sprawdz_kradziona())
 	bool czy_skradziona();
 	//prosi uzytkownika o podanie pinu 3x jezeli jest poprawne zwraca true, jezeli false połyka karte
@@ -26,15 +26,15 @@ public:
 	int sprawdz_saldo();
 	//wypłaca pieniądze z karty w krokach:
 	//1. prosi o wprowadzenie kwoty
-	//2. sprawdza dostępność salda (karta.get_saldo) (jesli nie ma podaje saldo i prosi o podanie innej kwoty)
-	//2* sprawdza dostępność gotowki w bankomacie
+	//2. sprawdza dostępnosc salda (karta.get_saldo) (jesli nie ma podaje saldo i prosi o podanie innej kwoty)
+	//2* sprawdza dostępnosc gotowki w bankomacie
 	//3. odejmuje kwote wypłacaną od salda
 	//3* odejmuje kwote wypłacaną od gotowki
 	//4. ustawia nowe saldo na karcie (karta.set_saldo)
 	//5. zapisuje dane karty do pliku
 	void wyplac();
-	bool odczyt();  // bool - żeby sprawdził czy dana karta istnieje
-	void zapis();
+	bool odczyt(int card_num);  // bool - zeby sprawdził czy dana karta istnieje
+	void zapis(int card_num);
 
 
 };
